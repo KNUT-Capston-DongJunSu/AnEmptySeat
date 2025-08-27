5bb2550596c1b86965afa1be69ef5bf014bf26b7
joon0890 <kimjunhee2483@gmail.com>
2025-08-27 14:18:33 +0900

    개발 세팅

---
+++ src/config/settings/components/apps.py
@@ -0,0 +1,12 @@
+INSTALLED_APPS = [
+    'corsheaders',
+    'django_extensions',
+    'storages',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework_simplejwt",
+]
