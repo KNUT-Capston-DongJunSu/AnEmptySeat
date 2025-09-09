92e6feeebead155adfbcc62d9625982670cac704
joon0890 <kimjunhee2483@gmail.com>
2025-09-09 15:00:34 +0900

    config 수정

--- src/config/settings/components/database.py
+++
@@ -1,9 +0,0 @@
-from decouple import config
-import dj_database_url
-
-DATABASES = {
-    'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
-        conn_max_age=600
-    )
-}
