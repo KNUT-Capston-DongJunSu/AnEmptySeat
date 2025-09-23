c8823d99e25d148f0480719c2eae543791ea9b84
hyunsu.ryu <abba010327@gmail.com>
2025-09-23 16:38:55 +0900

    first-fixed

---
+++ front/script.js
@@ -0,0 +1,14 @@
+const clockElement = document.getElementById('clock');
+
+function updateClock() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const date = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    clockElement.textContent = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
+}
+setInterval(updateClock, 1000);
+updateClock();
