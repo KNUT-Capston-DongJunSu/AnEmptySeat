2aeff7ad29e1dc9fd42488ca91f6da31476c6c3c
joon0890 <kimjunhee2483@gmail.com>
2025-09-30 18:09:27 +0900

    2

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -12,10 +12,10 @@ from .components.logging import *
 from .components.templates import *
 from .components.static import *
 
-ROOT_URLCONF = "Ssrc.config.urls"
+ROOT_URLCONF = "src.config.urls"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-WSGI_APPLICATION = "Ssrc.config.wsgi.application"
-ASGI_APPLICATION = "Ssrc.config.asgi.application"
+WSGI_APPLICATION = "src.config.wsgi.application"
+ASGI_APPLICATION = "src.config.asgi.application"
 LANGUAGE_CODE = "ko-kr"
 TIME_ZONE = "Asia/Seoul"
 USE_I18N = True
