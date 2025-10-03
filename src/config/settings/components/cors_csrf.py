cc244906db662712086ee1dcb51c990da01de497
joon0890 <kimjunhee2483@gmail.com>
2025-10-03 15:30:28 +0900

    쉽표 빠짐 수정

--- src/config/settings/components/cors_csrf.py
+++ src/config/settings/components/cors_csrf.py
@@ -4,8 +4,8 @@ CORS_ALLOWED_ORIGINS = [
 ALLOWED_HOSTS = [
     '54.196.225.37', 
     '50.16.92.195',
-    'cafe-empty.duckdns.org'
-    'cafe-empty.duckdns.org:8000'
+    'cafe-empty.duckdns.org',
+    'cafe-empty.duckdns.org:8000',
     'localhost', 
     '127.0.0.1'
     ]
